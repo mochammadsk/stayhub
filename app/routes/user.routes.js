8c0f57a7ba2ad4eb1e805d910ff60897a8ee9d7d
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-19 23:05:07 +0800

    add user routes

---
+++ app/routes/user.routes.js
@@ -0,0 +1,48 @@
+module.exports = (app) => {
+  const user = require("../controllers/user/user.controllers");
+  const { register } = require("../controllers/user/user.controllers");
+  const userService = require("../controllers/user/userAuth.controller");
+  const validateRegistration = require("../middelware/validateRegristation");
+  const { auth, isAdmin } = require("../middelware/subsAuth");
+  const r = require("express").Router();
+
+  // Register account
+  r.post("/signup", validateRegistration, (req, res) => {
+    const data = req.body;
+    register(data)
+      .then((result) => res.status(200).json(result))
+      .catch((error) => res.status(400).json(error));
+  });
+
+  // Verification email for register account
+  r.get("/verify/:uniqueString", user.verifyEmail);
+
+  // Login account
+  r.post("/signin", userService.handleLogin);
+
+  // Password reset
+  r.post("/forgot-password", (req, res) => {
+    user.resetPassword(req, res);
+  });
+
+  // Verification email for password rest
+  r.post("/verify", (req, res) => {
+    user.verifyResetPassword(req, res);
+  });
+
+  // Update data
+  r.put("/update/:id", auth, (req, res) => {
+    user.update(req, res);
+  });
+
+  // Delete data
+  r.delete("/delete/:id", auth, isAdmin, (req, res) => {
+    user.delete(req, res);
+  });
+
+  // Google Auth
+  r.get("/auth/google", user.googleAuthRedirect);
+  r.get("/auth/google/callback", user.googleAuthCallback);
+
+  app.use("/user", r);
+};
