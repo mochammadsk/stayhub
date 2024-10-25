c8fb0379cedc8f578dd85d84737370e8b8a596b1
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-26 05:27:47 +0800

    just update to auth routes

--- app/routes/public.routes.js
+++ app/routes/public.routes.js
@@ -1,17 +1,8 @@
 module.exports = (app) => {
   const router = require("express").Router();
 
-  // Public routes
   router.get("/", (req, res) => {
     res.render("home");
-  });
-
-  router.get("/signup", (req, res) => {
-    res.render("signup");
-  });
-
-  router.get("/signin", (req, res) => {
-    res.render("signin");
   });
 
   app.use("/", router);
