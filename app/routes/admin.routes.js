dc924a3921f26fba0db03d62d3adbe987801350d
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-26 05:26:09 +0800

    update routes

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -1,14 +1,10 @@
 module.exports = (app) => {
   const admin = require("../controllers/admin/admin.controllers");
-  const adminService = require("../controllers/admin/adminAuth.controllers");
-  const { auth, isAdmin } = require("../middelware/subsAuth");
+  const { auth } = require("../middelware/auth.middleware");
   const router = require("express").Router();
   const dotenv = require("dotenv");
 
   dotenv.config();
-
-  // Login account
-  router.post("/signin", adminService.handleLogin);
 
   // Logout account
   router.post("/logout", (req, res) => {
@@ -21,21 +17,21 @@ module.exports = (app) => {
   });
 
   // Show data user
-  router.get("/list/user", auth, isAdmin, (req, res) => {
+  router.get("/list/user", auth, (req, res) => {
     admin.findAll(req, res);
   });
 
-  router.get("/list/user/:id", auth, isAdmin, (req, res) => {
+  router.get("/list/user/:id", auth, (req, res) => {
     admin.show(req, res);
   });
 
   // Update data
-  router.put("/update/:id", auth, isAdmin, (req, res) => {
+  router.put("/update/:id", auth, (req, res) => {
     admin.update(req, res);
   });
 
   // Delete data
-  router.delete("/delete/:id", auth, isAdmin, (req, res) => {
+  router.delete("/delete/:id", auth, (req, res) => {
     admin.delete(req, res);
   });
 
