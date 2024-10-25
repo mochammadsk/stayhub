bbb72c012da74d8293fe6cd858a7f6fbdc50aa5d
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-26 05:25:03 +0800

    move logic login admin

--- app/controllers/admin/admin.controllers.js
+++ app/controllers/admin/admin.controllers.js
@@ -1,47 +1,8 @@
 const Admin = require("../../models/admin/admin.models");
 const User = require("../../models/user/user.models");
-const jwt = require("jsonwebtoken");
-const argon2 = require("argon2");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
-exports.login = async (data) => {
-  try {
-    // Find admin based on username
-    const admin = await Admin.findOne({ userName: data.userName });
-    if (!admin) {
-      console.error("Username not found:", data.userName);
-      throw new Error("Username not found!");
-    }
-
-    // Verify password
-    const match = await argon2.verify(admin.password, data.password);
-    if (!match) {
-      console.error("Wrong password for userName:", data.userName);
-      throw new Error("Wrong password!");
-    }
-
-    // Check status role
-    if (admin.role !== "admin") {
-      console.error("Unauthorized role for userName:", data.userName);
-      throw new Error("Unauthorized role!");
-    }
-
-    // Create JWT token
-    const token = jwt.sign(
-      { userName: admin.userName, role: admin.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    console.log("Login successful for userName:", data.userName);
-    return { message: "Login Successful", token };
-  } catch (error) {
-    console.error("Login error:", error.message);
-    throw new Error(error.message);
-  }
-};
 
 // Show data user
 exports.findAll = (req, res) => {
