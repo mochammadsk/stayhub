1f91131390fe3365db35caafb767f025baf0c58d
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-01 23:17:44 +0800

    update middleware

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require("express");
 const cors = require("cors");
-const flash = require("connect-flash");
-const session = require("express-session");
-const bodyParser = require("body-parser");
+const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const swaggerConfig = require("./app/config/swagger");
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-const swaggerConfig = require("./config/swagger");
 
 const app = express();
-const swaggerSpec = swaggerJSDoc(swaggerConfig);
-const corsOption = {
-  origin: "*",
-};
 
-// Middelware CORS and express
-app.use(flash());
+const swaggerSpec = swaggerJSDoc(swaggerConfig);
+
+// Middleware CORS & Parsing
 app.use(express.json());
-app.use(cors(corsOption));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    method: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  })
+);
 
 // Midelleware session
 app.use(
@@ -28,28 +30,19 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: 3600000,
+      maxAge: 60000,
       secure: false,
     },
   })
 );
 
-// Miiddleware body parser
-app.use(
-  bodyParser.json({
-    extended: true,
-    limit: "50mb",
-  })
-);
+// Middleware Logger
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-    limit: "50mb",
-  })
-);
-
-// Use EJS
+// EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -57,9 +50,9 @@ app.set("views", path.join(__dirname, "/views"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Call routes
-require("./routes/admin.routes")(app);
-require("./routes/auth.routes")(app);
-require("./routes/public.routes")(app);
-require("./routes/user.routes")(app);
+require("./app/routes/admin.routes")(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/public.routes")(app);
+require("./app/routes/user.routes")(app);
 
 module.exports = app;
