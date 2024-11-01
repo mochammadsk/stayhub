a4cfe66fe3b2b5c34feebc8d6f6332969bf5c6d6
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-02 00:37:43 +0800

    add schema

---
+++ app/swagger/shema.swagger.js
@@ -0,0 +1,57 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Auth:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Admin:
+ *       type: object
+ *       required:
+ *         - userName
+ *         - email
+ *         - password
+ *       properties:
+ *         userName:
+ *           type: string
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - fullName
+ *         - email
+ *         - phone
+ *         - password
+ *       properties:
+ *         fullName:
+ *           type: string
+ *         email:
+ *           type: string
+ *         phone:
+ *           type: number
+ *           format: int32
+ *         password:
+ *           type: string
+ */
