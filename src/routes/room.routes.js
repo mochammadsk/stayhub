9e5fc9dedf2d2563ee57b5a3885062e1369b0e2f
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-09 01:27:42 +0800

    fix role auth

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   const room = require('../controllers/room.controller');
   const { auth } = require('../middelware/auth.middleware');
-  const upload = require('../config/multer');
+  const { uploadRoomImages } = require('../config/multer');
   const router = require('express').Router();
   const dotenv = require('dotenv');
 
@@ -18,22 +18,22 @@ module.exports = (app) => {
   });
 
   // Create room
-  router.post('/add', auth('admin'), upload, (req, res) => {
+  router.post('/add', auth(), uploadRoomImages, (req, res) => {
     room.addRoom(req, res);
   });
 
   // Update room by id
-  router.put('/update/:id', auth('admin'), (req, res) => {
+  router.put('/update/:id', auth(), uploadRoomImages, (req, res) => {
     room.updateRoom(req, res);
   });
 
   // Delete room by id
-  router.delete('/delete/:id', auth('admin'), (req, res) => {
+  router.delete('/delete/:id', auth(), (req, res) => {
     room.deleteById(req, res);
   });
 
   // Delete all rooms
-  router.delete('/delete', auth('admin'), (req, res) => {
+  router.delete('/delete', auth(), (req, res) => {
     room.deleteAll(req, res);
   });
 
