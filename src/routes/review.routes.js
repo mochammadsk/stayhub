8f0de8821cc6945c29ee0447b22da4b2df343a09
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-12 07:04:14 +0800

    initiation with add routes to post review room

---
+++ src/routes/review.routes.js
@@ -0,0 +1,12 @@
+module.exports = (app) => {
+  const review = require('../controllers/review.controller');
+  const { auth } = require('../middelware/auth.middleware');
+  const router = require('express').Router();
+
+  // Create review
+  router.post('/review/:id', auth('user'), (req, res) => {
+    review.addReview(req, res);
+  });
+
+  app.use('/room', router);
+};
