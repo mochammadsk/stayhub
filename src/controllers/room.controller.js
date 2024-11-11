cc41f5274257de20a309933d7823f3bf2f79413a
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-12 02:16:06 +0800

    fix update room

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -62,15 +62,24 @@ exports.addRoom = async (req, res) => {
 exports.updateRoom = async (req, res) => {
   try {
     const { type, name, cost } = req.body;
-    const roomId = req.params.id;
-    const room = await Room.findById(roomId);
+    const room = await Room.findById(req.params.id);
+
     if (!room) {
+      if (req.files && req.files.length > 0) {
+        await Promise.all(req.files.map((file) => fs.unlink(file.path)));
+      }
       return res.status(404).json({ message: 'Room not found' });
     }
 
     const existingRoom = await Room.findOne({ name });
+
     if (existingRoom) {
-      return res.status(404).json({ message: 'Room already exists' });
+      if (req.files && req.files.length > 0) {
+        await Promise.all(req.files.map((file) => fs.unlink(file.path)));
+      }
+      return res
+        .status(409)
+        .json({ message: 'Room with the same name already exists' });
     }
 
     room.type = type || room.type;
@@ -78,11 +87,9 @@ exports.updateRoom = async (req, res) => {
     room.cost = cost || room.cost;
 
     if (req.files && req.files.length > 0) {
-      room.images.forEach((image) => {
-        fs.unlink(image.url, (err) => {
-          console.log(err);
-        });
-      });
+      await Promise.all(
+        room.images.map((image) => fs.unlink(image.url).catch(console.error))
+      );
 
       const images = req.files.map((file) => ({
         url: file.path,
@@ -147,7 +154,6 @@ exports.deleteAll = async (req, res) => {
     await Room.deleteMany();
     res.status(200).json({ message: 'All rooms deleted!' });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
