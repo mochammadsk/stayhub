f52c4d8e26e07c2bea7a19dcdd1cf6bdb5bca6f8
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-12 07:03:38 +0800

    initiation

---
+++ src/models/review.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+reviewSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
