3d9d389f69f70a3d939fcb2588c14a180cd991d2
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-14 17:54:17 +0800

    add delete review in controller

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -73,3 +73,33 @@ exports.updateReview = async (req, res) => {
     res.status(500).json({ message: 'Internal server eror', error });
   }
 };
+
+// Delete review
+exports.deleteReview = async (req, res) => {
+  try {
+    if (!req.user || !req.user.id) {
+      return res.status(400).json({ message: 'User ID is missing' });
+    }
+
+    const room = await Room.findById(req.params.id).populate('reviews');
+    if (!room) {
+      return res.status(404).json({ message: 'Room not found' });
+    }
+
+    const review = await Review.findById(room.reviews[0]._id);
+    if (!review) {
+      return res.status(404).json({ message: 'Review not found' });
+    }
+
+    await Review.findByIdAndDelete(review._id);
+
+    room.reviews = room.reviews.filter(
+      (r) => r._id.toString() !== review._id.toString()
+    );
+    await room.save();
+
+    res.status(200).json({ message: 'Review deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error', error });
+  }
+};
