7794d05b3f46af6b83199b0b96fed30b164bbd89
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-17 00:01:11 +0800

    add schema req user_id for for connect with data user

--- src/models/complaint.model.js
+++ src/models/complaint.model.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const complaintSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     title: { type: String, required: true },
     description: { type: String, required: true },
     status: {
