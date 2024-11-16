8613a3614d0688db38f541b749b17ff79e75cd8f
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-16 08:58:22 +0800

    update for update profile to add images profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,20 +1,48 @@
 const User = require('../models/user.model');
 const dotenv = require('dotenv');
+const fs = require('fs').promises;
+const path = require('path');
 
 dotenv.config();
 
-// Update data
-exports.update = (req, res) => {
-  const id = req.params.id;
+// Update profile
+exports.updateProfile = async (req, res) => {
+  try {
+    const { fullName, email, phone } = req.body;
 
-  User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then((data) => {
-      if (!data) {
-        res.status(404).send({ messages: "Data can't be updated!" });
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+    user.phone = phone || user.phone;
+
+    if (req.file) {
+      if (Array.isArray(user.images) && req.file.length > 0) {
+        for (const image of user.images) {
+          const filePath = path.resolve(image.url);
+          if (fs.existsSync(filePath)) {
+            console.log(`Deleted old file: ${filePath}`);
+            await fs.unlink(filePath);
+          }
+        }
       }
-      res.send({ messages: 'Data updated successfully!' });
-    })
-    .catch((err) => res.status(500).send({ messages: err.messages }));
+      user.images = [
+        {
+          url: req.file.path,
+          filename: req.file.filename,
+        },
+      ];
+    }
+
+    await user.save();
+    res.status(200).json({ message: 'Profile updated successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal Server Error', error });
+  }
 };
 
 // Delete data
