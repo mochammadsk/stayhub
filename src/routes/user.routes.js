681a733bf3186322c4ad1d7b7cdd0351ce91554a
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-16 08:54:09 +0800

    delete logic do not use, and fix for update profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,18 @@
 module.exports = (app) => {
   const user = require('../controllers/user.controller');
-  const userService = require('../controllers/userAuth.controller');
   const { auth } = require('../middelware/auth.middleware');
+  const { uploadProfileImages } = require('../config/multer');
   const router = require('express').Router();
 
-  // Login account
-  router.post('/signin', userService.handleLogin);
-
-  // Logout account
-  router.post('/logout/:id', (req, res) => {
-    req.session.destroy((err) => {
-      if (err) {
-        return res.status(500).json({ message: 'Logout failed!' });
-      }
-      res.status(200).json({ message: 'Logged out successfully!' });
-    });
-  });
-
   // Update profile data
-  router.put('/profile/update/:id', auth, (req, res) => {
-    user.update(req, res);
-  });
+  router.put(
+    '/profile/update',
+    auth('user'),
+    uploadProfileImages,
+    (req, res) => {
+      user.updateProfile(req, res);
+    }
+  );
 
   // Delete data
   router.delete('/delete/:id', auth, (req, res) => {
