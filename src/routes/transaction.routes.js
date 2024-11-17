ff81e809f298b16dea31e3c8ea9713c5eb400480
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-18 03:21:22 +0800

    add create routes

---
+++ src/routes/transaction.routes.js
@@ -0,0 +1,17 @@
+module.exports = (app) => {
+  const transaction = require('../controllers/transaction.controller');
+  const { auth } = require('../middelware/auth.middleware');
+  const router = require('express').Router();
+
+  // Create transaction
+  router.post('/callback/:id', auth('user'), (req, res) => {
+    transaction.create(req, res);
+  });
+
+  // Update transaction
+  router.put('/update/:id', auth('admin'), (req, res) => {
+    transaction.update(req, res);
+  });
+
+  app.use('/transaction', router);
+};
