c5feb8a69ef951149f18be7ff508b79754bf327d
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-25 22:06:13 +0800

    adapt from facility

--- src/controllers/typeRoom.controller.js
+++ src/controllers/typeRoom.controller.js
@@ -1,13 +1,17 @@
 const TypeRoom = require('../models/typeRoom.model');
-const path = require('path');
-const fs = require('fs').promises;
+const Room = require('../models/room.model');
 
-// Get all typeRoom
+// Get all type room
 exports.getAll = async (req, res) => {
-  const typeRoom = await TypeRoom.find();
   try {
+    const typeRoom = await TypeRoom.find().populate({
+      path: 'facility',
+      select: 'name',
+    });
+
+    // Check if type room exist
     if (typeRoom.length === 0) {
-      return res.status(404).json({ message: 'TypeRoom not found' });
+      return res.status(404).json({ message: 'Data not found' });
     }
 
     res.status(200).json({ data: typeRoom });
@@ -16,128 +20,94 @@ exports.getAll = async (req, res) => {
   }
 };
 
-exports.getOne = async (req, res) => {
+// Get one type room
+exports.getById = async (req, res) => {
   const id = req.params.id;
   try {
+    // Check data exist
     const typeRoom = await TypeRoom.findById(id);
     if (!typeRoom) {
-      return res.status(404).json({ message: 'TypeRoom not found' });
+      return res.status(404).json({ message: 'Data not found' });
     }
 
-    res.status(200).json({ message: 'Type room found', data: typeRoom });
+    res.status(200).json({ message: 'Data found', data: typeRoom });
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
 
-// Create typeRoom
+// Create type room
 exports.create = async (req, res) => {
-  const { type, facility, cost } = req.body;
+  const { name, description, cost } = req.body;
   try {
-    // Check if typeRoom already exists
-    const existingReview = await TypeRoom.findOne({ type });
+    // Check data exists
+    const existingReview = await TypeRoom.findOne({ name });
     if (existingReview) {
       return res.status(404).json({ message: 'Type already exists' });
     }
 
-    // Upload images
-    const images = req.files.map((file) => ({
-      url: file.path,
-      filename: file.filename,
-    }));
+    // Create data
+    const type = new TypeRoom({ name, description, cost });
+    await type.save();
 
-    // Create typeRoom
-    const typeRoom = new TypeRoom({ type, facility, cost, images });
-    await typeRoom.save();
-
-    res.status(201).json({ data: typeRoom });
+    res.status(201).json({ messages: 'Created successfully', data: type });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
 
 // Update typeRoom
 exports.update = async (req, res) => {
-  const { type, facility, cost } = req.body;
+  const { name, description, cost } = req.body;
   try {
-    const typeRoom = await TypeRoom.findById(req.params.id);
-    if (!typeRoom) {
-      // Delete images if room not found
-      if (req.files && req.files.length > 0) {
-        await Promise.all(req.files.map((file) => fs.unlink(file.path)));
-      }
-      return res.status(404).json({ message: 'TypeRoom not found' });
+    // Check if data exists
+    const type = await TypeRoom.findById(req.params.id);
+    if (!type) {
+      return res.status(404).json({ message: 'Data not found' });
     }
 
-    const existingType = await TypeRoom.findOne({ type });
+    // Check if type room name already exists
+    const existingType = await TypeRoom.findOne({ name });
     if (existingType) {
-      // Delete images if room with the same name already exists
-      if (req.files && req.files.length > 0) {
-        await Promise.all(req.files.map((file) => fs.unlink(file.path)));
-      }
       return res
         .status(409)
-        .json({ message: 'Room with the same name already exists' });
+        .json({ message: 'Type room with the same name already exists' });
     }
 
     // Update data
-    typeRoom.type = type || typeRoom.type;
-    typeRoom.facility = facility || typeRoom.facility;
-    typeRoom.cost = cost || typeRoom.cost;
+    type.name = name || type.name;
+    type.description = description || type.description;
+    type.cost = cost || type.cost;
 
-    if (req.files && req.files.length > 0) {
-      // Delete old images
-      if (typeRoom.images && typeRoom.images.length > 0) {
-        for (const image of typeRoom.images) {
-          const filePath = path.resolve(image.url);
+    // Save data
+    await type.save();
 
-          await fs.access(filePath);
-          await fs.unlink(filePath);
-        }
-      }
-      // Update images
-      typeRoom.images = req.files.map((file) => ({
-        url: file.path,
-        filename: file.filename,
-      }));
-    }
-
-    // Save Type Room
-    await typeRoom.save();
-
-    res.status(200).json({ data: typeRoom });
+    res.status(200).json({ message: 'Data updated successfully', data: type });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
 
 // Delete one typeRoom
-exports.deleteOne = async (req, res) => {
+exports.deleteById = async (req, res) => {
   try {
-    const typeRoom = await TypeRoom.findById(req.params.id);
-    if (!typeRoom) {
-      return res.status(404).json({ message: 'TypeRoom not found' });
+    // Check if data exists
+    const type = await TypeRoom.findById(req.params.id);
+    if (!type) {
+      return res.status(404).json({ message: 'Data not found' });
     }
 
-    // Delete images
-    const deleteImages = typeRoom.images.map((typeRoom) => {
-      const filePath = path.resolve(
-        __dirname,
-        '../../public/images/rooms',
-        image.filename
-      );
-      return fs.unlink(filePath);
-    });
-    await Promise.all(deleteImages);
+    // Remove references from Room documents
+    await Room.updateMany(
+      { type: req.params.id },
+      { $pull: { type: req.params.id } }
+    );
 
-    // Delete typeRoom
+    // Delete data
     await TypeRoom.findByIdAndDelete(req.params.id);
 
-    res.status(200).json({ message: 'Type Room deleted!' });
+    res.status(200).json({ message: 'Data deleted successfully' });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
@@ -148,28 +118,24 @@ exports.deleteAll = async (req, res) => {
   try {
     // Check if type exist
     if (typeRoom.length === 0) {
-      return res.status(404).json({ message: 'TypeRoom not found' });
+      return res.status(404).json({ message: 'Data not found' });
     }
 
-    // Delete images
-    const deleteImages = typeRoom.flatMap((typeRoom) =>
-      typeRoom.images.map((image) => {
-        const filePath = path.resolve(
-          __dirname,
-          '../../public/images/rooms',
-          image.filename
-        );
-        return fs.unlink(filePath);
-      })
-    );
-    await Promise.all(deleteImages);
+    // Get type room id
+    const typeRooms = await TypeRoom.find();
+    const typeRoomId = typeRooms.map((typeRoom) => typeRoom._id);
 
-    // Delete typeRoom
+    // Remove references from Room documents
+    await Room.updateMany(
+      { type: { $in: typeRoomId } },
+      { $pull: { type: { $in: typeRoomId } } }
+    );
+
+    // Delete data
     await TypeRoom.deleteMany();
 
     res.status(200).json({ message: 'Data deleted successfully' });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
