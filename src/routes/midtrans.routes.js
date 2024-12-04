ee30b50f62dcff581f7126742c2c7c80dbd34049
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-05 03:33:58 +0800

    fix routes for webhook

--- src/routes/midtrans.routes.js
+++ src/routes/midtrans.routes.js
@@ -1,13 +1,11 @@
-/* NEXT DEVELOPMENT */
+module.exports = (app) => {
+  const midtrans = require('../handlers/midtrans.handler');
+  const router = require('express').Router();
 
-// module.exports = (app) => {
-//   const midtrans = require('../handlers/midtrans.handler');
-//   const router = require('express').Router();
+  // Webhook midtrans
+  router.post('/webhook', (req, res) => {
+    midtrans.webhook(req, res);
+  });
 
-//   // Webhook midtrans
-//   router.post('/webhook', (req, res) => {
-//     midtrans.webhook(req, res);
-//   });
-
-//   app.use('/', router);
-// };
+  app.use('/', router);
+};
