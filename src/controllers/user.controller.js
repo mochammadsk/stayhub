16e13bea487ef9fdb1cb23b8ed8f95208e099f38
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-10 17:32:18 +0800

    add get user profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,20 @@ const fs = require('fs').promises;
 
 dotenv.config();
 
+// Get profile
+exports.getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ data: user });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error', error });
+  }
+};
+
 // Update profile
 exports.updateProfile = async (req, res) => {
   const { fullName, email, phone, address } = req.body;
